c8d43da42f5b87a4d9fc84fad592f1a2e69e57fc
brian-ds <brian@luxe.com>
2016-10-04 01:28:33 -0700

    remove my real settings

--- attbillsplitter/settings.py
+++ attbillsplitter/settings.py
@@ -2,31 +2,31 @@
 """Configs used in the main module."""
 
 # twilio
-twilio_number = '+13103128668'
-twilio_account_sid = 'ACcfa45113ae5bfb96fb78afe54c8a9075'
-twilio_auth_token = '358d0dd19d428769ae97cf99388d62e4'
+twilio_number = 'your_twilio_number'
+twilio_account_sid = 'your_twilio_api_key_here'
+twilio_auth_token = 'your_twilio_auth_token'
 
 # AT&T account credentials
-username = 'cfnczq@att.net'
-password = 'Zq199156'
+username = 'your_att_user_name'
+password = 'your_att_password'
 
 # phonebook with names and numbers
 # name and number should match with user info on AT&T website
 # number in format of '123-456-7890'
 phonebook = [
-    ('QIANG ZHANG', '310-849-5586'),
-    ('QIANG ZHANG', '213-379-3374'),
-    ('WENYAN MIAO', '424-901-3196'),
-    ('QIMING SHAO', '310-666-8391'),
-    ('KAIYU CHEN', '310-721-2583'),
-    ('JIA HE', '310-733-7105'),
-    ('QIANG ZHANG', '408-386-4029'),
-    ('QIANG ZHANG', '424-230-1207'),
-    ('XU WU', '424-901-3008'),
-    ('BAOLIN YI', '424-901-3063'),
+    ('account_holder_name', 'account_holder_number'),
+    ('user_1_name', 'user_1_number'),
+    ('user_2_name', 'user_2_number'),
+    ('user_3_name', 'user_3_number'),
+    ('user_4_name', 'user_4_number'),
+    ('user_5_name', 'user_5_number'),
+    ('user_6_name', 'user_6_number'),
+    ('user_7_name', 'user_7_number'),
+    ('user_8_name', 'user_8_number'),
+    ('user_9_name', 'user_9_number'),
 ]
 
 # config
 # number of seconds to wait for the billing page to load
-# increase this number if your internet is slow
+# increase this number if the internet is slow
 page_loading_wait_s = 10
